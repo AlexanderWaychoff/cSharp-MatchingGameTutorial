407d6ca90b8c86f6d07edeaaf6bbf22aa593fd5a
Alex Waychoff <AlexWaychoff@gmail.com>
2017-12-08 14:25:44 -0600

    added click method and set labels to null to flag show/hide symbols

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,6 +12,14 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        // firstClicked points to the first Label control 
+        // that the player clicks, but it will be null 
+        // if the player hasn't clicked a label yet
+        Label firstClicked = null;
+
+        // secondClicked points to the second Label control 
+        // that the player clicks
+        Label secondClicked = null;
         // Use this Random object to choose random icons for the squares
         Random random = new Random();
 
@@ -61,9 +69,20 @@ namespace MatchingGame
                 // an icon that's already been revealed --
                 // ignore the click
                 if (clickedLabel.ForeColor == Color.Black)
+                {
                     return;
+                }
+                // If firstClicked is null, this is the first icon 
+                // in the pair that the player clicked,
+                // so set firstClicked to the label that the player 
+                // clicked, change its color to black, and return
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
 
-                clickedLabel.ForeColor = Color.Black;
+                    return;
+                }
             }
         }
     }
